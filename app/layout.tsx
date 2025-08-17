9b2960fa54a44000ededc6ecaadb9a5d9733bdb3
Mike Whelan <43212748+MikeWhelan91@users.noreply.github.com>
2025-08-17 16:50:29 +0100

    Enable analytics and ad storage by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -115,10 +115,10 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('consent', 'default', {
-                ad_user_data: 'denied',
-                ad_personalization: 'denied',
-                ad_storage: 'denied',
-                analytics_storage: 'denied'
+                ad_user_data: 'granted',
+                ad_personalization: 'granted',
+                ad_storage: 'granted',
+                analytics_storage: 'granted'
               });
             `,
           }}
